30bbf7978a61040c98f5fc24eb0076fe05162742
nogelscha <55446447+nogelscha@users.noreply.github.com>
2021-01-14 11:51:54 +0100

    Update contactform.php

--- contactform.php
+++ contactform.php
@@ -5,7 +5,7 @@ $name =$_POST[’name’]
 $subject =$_POST[’subject’]
 $mailfrom =$_POST[’mail’]
 $message =$_POST[’message’]
-$Abreise =$_POST[’Abreise]
+$Abreise =$_POST[’Abreise’]
 $Anreise =$_PSOT[’Anreise’]
 
 $mailTo = "bio1@utanet.at;
