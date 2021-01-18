2166db8487aefef5347da185d6e31bd37fb136b0
nogelscha <55446447+nogelscha@users.noreply.github.com>
2021-01-18 15:11:06 +0100

    Update contactform.php

--- contactform.php
+++ contactform.php
@@ -5,10 +5,9 @@ $name =$_POST[’name’]
 $subject =$_POST[’subject’]
 $mailfrom =$_POST[’mail’]
 $message =$_POST[’message’]
-$Abreise =$_POST[’Abreise’]
-$Anreise =$_PSOT[’Anreise’]
 
-$mailTo = jrettenwender@gmail.com;
+
+$mailTo = "jrettenwender@gmail.com";
 $headers = " From: ". $mailfrom;
 $txt = "you have recived an email from".$name."Arrival is".$Anreise."Departure is".$Abreise.".\n\n".$message;
 
